9430f6e92ae4d903018a50632c605ae2fed63815
adi-gupta <aditya.gupta@vresorts.in>
2018-02-08 20:58:46 +0530

    slider add

--- www/app/js/app/controllers/stTestCtrl.js
+++ www/app/js/app/controllers/stTestCtrl.js
@@ -4,28 +4,47 @@
   Aditya Gupta
 */
 angular.module('studentApp').controller('stTestCtrl',['$scope','$rootScope','$location','$timeout','$cookies', function ($scope,$rootScope,$location,$timeout,$cookies) {
-  document.getElementById("defaultOpen").click();
+   //document.getElementById("defaultOpen").click();
+
+   $scope.upcomingTest = true;
+
+   console.log($scope.upcomingTest);
+   $scope.openTestSchedule = function(testType) {
+        console.log(testType);
+        if(testType == 'upcomingTest'){
+            $scope.upcomingTest = true;
+        }else{
+            $scope.upcomingTest = false;    
+        }
+    }
+    $('.uptest-info').slick({
+         centerMode: false,
+  centerPadding: '0px',
+  slidesToShow: 1,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        infinite: false,
+        centerMode: true,
+        centerPadding: '50px',
+        slidesToShow: 1,
+        variableWidth: false
+      }
+    }
+  ]
+    });
 }]);
 
 	
-	function openTestSchedule(evt, testType) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
-
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(testType).style.display = "block";
-    evt.currentTarget.className += " active";
-}
 
