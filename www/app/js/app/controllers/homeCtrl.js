aa59bb00b6d2bdcaee302911d9e9b027440c02b8
himanshu-vr <himanshu.gupta@vresorts.in>
2018-02-13 00:32:01 +0530

    api integration

--- www/app/js/app/controllers/homeCtrl.js
+++ www/app/js/app/controllers/homeCtrl.js
@@ -3,10 +3,26 @@
   controller for managing the login activities of student and teachers
   Aditya Gupta
 */
-angular.module('studentApp').controller('homeCtrl',['$scope','$rootScope','$location','$timeout','$cookies', function ($scope,$rootScope,$location,$timeout,$cookies) {
+angular.module('studentApp').controller('homeCtrl',['$scope','$rootScope','$location','$timeout','$cookies','studentService', function ($scope,$rootScope,$location,$timeout,$cookies,studentService) {
 
-  //var data = $cookies.get('access_token');
-  //console.log(JSON.parse(data));
+  //init videos and other details
+  $scope.init =  function(){
+    //get Recommended videos
+    studentService.recommendedVideo()
+      .then(function onSuccess(response) {
+        if(response != undefined && typeof(response) == 'object'){
+          if(response.data != undefined && response.data.length > 0){
+            $scope.popularVideos = response.data;
+          }
+        }else{
+        }
+      })
+      .catch(function onError(errorResponse) {
+
+      })
+      .finally(function eitherWay(){
+      })
+  }
   $scope.lecture = function(){
   	$location.path('/student/lecture');
   }
@@ -17,46 +33,62 @@ angular.module('studentApp').controller('homeCtrl',['$scope','$rootScope','$loca
     $cookies.remove("access_token");
     $location.path('/login');
   }
-
-
+  /* Set the width of the side navigation to 250px */
+   $scope.openNav =  function() {
+      document.getElementById("mySidenav").style.width = "300px";
+  }
+  /* Set the width of the side navigation to 0 */
+  $scope.closeNav =  function() {
+      document.getElementById("mySidenav").style.width = "0";
+  }
   $('#popular_videos').slick({
-         centerMode: false,
-  centerPadding: '0px',
-  slidesToShow: 1,
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 3
+    centerMode: false,
+    centerPadding: '0px',
+    slidesToShow: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          infinite: false,
+          centerMode: true,
+          centerPadding: '50px',
+          slidesToShow: 1,
+          variableWidth: false
+        }
       }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        arrows: false,
-        infinite: false,
-        centerMode: true,
-        centerPadding: '50px',
-        slidesToShow: 1,
-        variableWidth: false
+    ]
+      });
+  //Play a video
+  $scope.pauseOrPlay = function(ele){
+    var video = angular.element(ele.srcElement);
+    if(video != undefined){
+      if(video[0].paused){
+        if (video[0].requestFullscreen) {
+             video[0].requestFullscreen();
+         }
+         else if (video[0].msRequestFullscreen) {
+             video[0].msRequestFullscreen();
+         }
+         else if (video[0].mozRequestFullScreen) {
+             video[0].mozRequestFullScreen();
+         }
+         else if (video[0].webkitRequestFullScreen) {
+             video[0].webkitRequestFullScreen();
+         }
+        video[0].play();
+      }else{
+        video[0].pause();
       }
     }
-  ]
-    });
-
+  }
 }]);
-
-   
-
-  /* Set the width of the side navigation to 250px */
-function openNav() {
-    document.getElementById("mySidenav").style.width = "300px";
-}
-
-/* Set the width of the side navigation to 0 */
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
--- www/app/js/app/controllers/stTestCtrl.js
+++ www/app/js/app/controllers/stTestCtrl.js
@@ -3,48 +3,38 @@
   controller for managing the login activities of student and teachers
   Aditya Gupta
 */
-angular.module('studentApp').controller('stTestCtrl',['$scope','$rootScope','$location','$timeout','$cookies', function ($scope,$rootScope,$location,$timeout,$cookies) {
-   //document.getElementById("defaultOpen").click();
+angular.module('studentApp').controller('stTestCtrl',['$scope','$rootScope','$location','$timeout','$cookies','studentService', function ($scope,$rootScope,$location,$timeout,$cookies,studentService) {
+   $scope.init = function(){
+     $scope.upcomingTest = true;
+     $scope.attemptedTest = false;
+     //get test of students
+     studentService.testSchedule()
+       .then(function onSuccess(response) {
+         if(response != undefined && typeof(response) == 'object'){
+           if(response.data != undefined && response.data.length > 0){
+              $scope.tests = response.data;
+              console.log($scope.tests);
+           }
+         }else{
+         }
+       })
+       .catch(function onError(errorResponse) {
 
-   $scope.upcomingTest = true;
-
-   console.log($scope.upcomingTest);
+       })
+       .finally(function eitherWay(){
+       })
+   }
    $scope.openTestSchedule = function(testType) {
-        console.log(testType);
         if(testType == 'upcomingTest'){
             $scope.upcomingTest = true;
+            $scope.attemptedTest = false;
         }else{
-            $scope.upcomingTest = false;    
+            $scope.upcomingTest = false;
+            $scope.attemptedTest = true;
+            //$scope.attemptedTest  = true;
         }
     }
-    $('.uptest-info').slick({
-         centerMode: false,
-  centerPadding: '0px',
-  slidesToShow: 1,
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 3
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        arrows: false,
-        infinite: false,
-        centerMode: true,
-        centerPadding: '50px',
-        slidesToShow: 1,
-        variableWidth: false
-      }
-    }
-  ]
-    });
+  $scope.goBack  = function(){
+    $location.path('/home');
+  }
 }]);
-
-	
-
