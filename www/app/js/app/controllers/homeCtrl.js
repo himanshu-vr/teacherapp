4eaad6e7dec9ff903fe84fb0cb4b6dc14f8f0679
adi-gupta <aditya.gupta@vresorts.in>
2018-02-12 21:37:31 +0530

    side nav menu

--- www/app/js/app/controllers/homeCtrl.js
+++ www/app/js/app/controllers/homeCtrl.js
@@ -9,4 +9,42 @@ angular.module('studentApp').controller('homeCtrl',['$scope','$rootScope','$loca
   $scope.lecture = function(){
   	$location.path('/student/lecture');
   }
+
+    $('#popular_videos').slick({
+         centerMode: false,
+  centerPadding: '0px',
+  slidesToShow: 1,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        infinite: false,
+        centerMode: true,
+        centerPadding: '50px',
+        slidesToShow: 1,
+        variableWidth: false
+      }
+    }
+  ]
+    });
 }]);
+
+  /* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "300px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
