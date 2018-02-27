1cfb6e11e4aff3d9e9409b55f477fff4adfa4887
adi-gupta <aditya.gupta@vresorts.in>
2018-02-27 23:10:47 +0530

    few changes

--- www/app/js/app/controllers/stTestCtrl.js
+++ www/app/js/app/controllers/stTestCtrl.js
@@ -176,24 +176,24 @@ $scope.prevQuestion = function(){
   }
 }
 
-// Get the modal
-var modal = document.getElementById('submit_Modal');
+// // Get the modal
+// var modal = document.getElementById('submit_Modal');
 
-// Get the button that opens the modal
-var btn = document.getElementById("submit_Btn");
+// // Get the button that opens the modal
+// var btn = document.getElementById("submit_Btn");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
+// // When the user clicks on the button, open the modal 
+// btn.onclick = function() {
+//     modal.style.display = "block";
+// }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//     modal.style.display = "none";
+// }
 
 // // When the user clicks anywhere outside of the modal, close it
 // window.onclick = function(event) {
