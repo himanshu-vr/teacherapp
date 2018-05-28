6ce9796f1492d8683bcef8ed3d60905be5fbc103
adi-gupta <aditya.gupta@vresorts.in>
2018-05-29 00:24:51 +0530

    upload documents tab

--- www/app/js/app/controllers/documentCtrl.js
+++ www/app/js/app/controllers/documentCtrl.js
@@ -11,6 +11,8 @@ angular.module('studentApp').controller('documentCtrl',['$scope','$rootScope','$
   }
 
   $scope.init = function(){
+
+  	
     studentService.getalluploadeddocuments()
       .then(function onSuccess(response) {
         if(response != undefined && typeof(response) == 'object'){
@@ -28,4 +30,19 @@ angular.module('studentApp').controller('documentCtrl',['$scope','$rootScope','$
       .finally(function eitherWay(){
       })
   }
+
+  $scope.documentTab = function(testType) {
+  	$scope.uploadDoc = true;
+     $scope.downloadDoc = false;
+  if(testType == 'uploadDoc'){
+    $scope.uploadDoc = true;
+    angular.element(document.querySelector("#uptest-btn1")).addClass("testbtn-active");
+    angular.element(document.querySelector("#uptest-btn2")).removeClass("testbtn-active");
+    }else{
+	    $scope.uploadDoc = false;
+	    angular.element(document.querySelector("#uptest-btn1")).removeClass("testbtn-active");
+	    angular.element(document.querySelector("#uptest-btn2")).addClass("testbtn-active");
+            //$scope.attemptedTest  = true;
+   }
+}
 }]);
